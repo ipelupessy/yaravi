e2c0c0f9271ed99068dc9dda06174262fc4de489
F.I. Pelupessy <f.i.pelupessy@casema.nl>
2014-11-14 21:35:56 +0100

    consistent dps conversion

--- mp.py
+++ mp.py
@@ -1,6 +1,26 @@
 import math
 
-BACKEND="gmpy2"
+BACKEND="mpmath"
+
+#matches mpmath
+def prec_to_dps(n):
+    """Return number of accurate decimals that can be represented
+    with a precision of n bits."""
+    return max(1, int(round(int(n)/3.3219280948873626)-1))
+
+def dps_to_prec(n):
+    """Return the number of bits required to represent n decimals
+    accurately."""
+    return max(1, int(round((int(n)+1)*3.3219280948873626)))
+
+def repr_dps(n):
+    """Return the number of decimal digits required to represent
+    a number with n-bit precision so that it can be uniquely
+    reconstructed from the representation."""
+    dps = prec_to_dps(n)
+    if dps == 15:
+        return 17
+    return dps + 3
 
 if BACKEND=="mpmath":
     from mpmath import *
@@ -22,13 +42,13 @@ elif BACKEND=="gmpy2":
         
     def set_dps(new_dps):
       global dps
-      get_context().precision = int(log2(10.) * int(new_dps))    
+      get_context().precision = dps_to_prec(new_dps)    
       dps=new_dps
 
     def set_precision(new_precision):
       get_context().precision = new_precision
-      dps=int(new_precision/log2(10.))
+      dps=prec_to_dps(get_context().precision)
 
-    dps=int(get_context().precision/log2(10.))
+    dps=prec_to_dps(get_context().precision)
 
   
